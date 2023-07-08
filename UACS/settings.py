3f83076beeb7301dffad70a21ddaa7173a0ff736
Daniel Momodu <momodufresh@gmail.com>
2023-07-08 11:45:47 +0100

    allowed hosts added in settings

--- UACS/settings.py
+++ UACS/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -90,6 +90,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'UACS.wsgi.application'
+
+CSRF_TRUSTED_ORIGINS = ['https://uacs.onrender.com',]
+
+
 
 
 # Database
