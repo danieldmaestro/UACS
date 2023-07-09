519a2743806df2cf86325b55b5b6b12fd4affb65
Daniel Momodu <momodufresh@gmail.com>
2023-07-09 20:41:20 +0100

    testing api response 2

--- uacs_app/utils.py
+++ uacs_app/utils.py
@@ -7,8 +7,16 @@ from .models import ServiceProvider, Staff, StaffPermission
 
 load_dotenv()
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    return (
+        x_forwarded_for.split(",")[0]
+        if x_forwarded_for
+        else request.META.get("REMOTE_ADDR")
+    )
+
 def get_user_location(request):
-    ip = request.META.get('REMOTE_ADDR')
+    ip = get_client_ip(request)
     response = requests.get(f"http://api.ipstack.com/{ip}?access_key={os.environ.get('API_KEY')}")
     print(response.status_code)
     if response.status_code == 200:
@@ -18,13 +26,7 @@ def get_user_location(request):
         return location
     return None
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
-    return (
-        x_forwarded_for.split(",")[0]
-        if x_forwarded_for
-        else request.META.get("REMOTE_ADDR")
-    )
+
 
 def create_permissions(staff):
     for sp_obj in ServiceProvider.objects.all():
